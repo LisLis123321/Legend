1c55d92e70448eab6a8a6ade4bc86173c701a78a
Aleksandr <hitrovaleks2014@gmail.com>
2023-03-06 23:52:35 +0300

    fixed

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int defaultMoney = 100;
         int bonus = 0;
-        int check = 1300;
+        int check = 1100;
         if(check > 1000) {
-            bonus = (check - 100) / defaultMoney;
+            bonus = check / defaultMoney;
             System.out.println("Ваш бонус = " + bonus + " руб");
         }
         else {
